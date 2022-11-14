20931ffbe7999dba0fd01883912d99c5f73debf1
Bharatha_Sachintha <bharatha.s@affiniti.biz>
2022-11-14 19:20:09 +0530

    initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,12 @@
+package org.example;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Author: Bharatha Sachintha
+ * Date: ${DAY}/${MONTH}/${YEAR}
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
